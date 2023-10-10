5c8fff14cb293499e9ca7baedc4db1604f5bf017
Matteo Bitussi <matteobitussi@gmail.com>
2023-10-10 11:16:29 +0200

    make scripts use remote images + added testplan-to-pdf build action

--- testplans/spid-cie-oidc/implementations/spid-cie-oidc-django/build_and_run.sh
+++ testplans/spid-cie-oidc/implementations/spid-cie-oidc-django/build_and_run.sh
@@ -5,12 +5,12 @@ cp docker-compose.yml ./spid-cie-oidc-django/
 cd spid-cie-oidc-django
 bash docker-prepare.sh
 
-# Unnecessary part if image hosted in github --------
-cd ../../../../../tools/i-mig-t
-#rm mig-t-beta-jar-with-dependencies.jar
-sudo docker build -t i-mig-t .
-cd ../../testplans/spid-cie-oidc/implementations/spid-cie-oidc-django/spid-cie-oidc-django/
-# Unnecessary part if image hosted in github --------
+# local build i-mig-t --------
+# cd ../../../../../tools/i-mig-t
+# rm mig-t-beta-jar-with-dependencies.jar
+# sudo docker build -t i-mig-t .
+# cd ../../testplans/spid-cie-oidc/implementations/spid-cie-oidc-django/spid-cie-oidc-django/
+# local build i-mig-t --------
 
 xhost +local:
 sudo docker compose up --remove-orphans
