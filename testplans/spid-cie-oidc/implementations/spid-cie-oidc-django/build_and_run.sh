89630b8535023e6f4f7c2d1694c72cde3f7b906d
marche271 <marchesini.eleonora@gmail.com>
2024-07-17 14:15:30 +0200

    Small updates

--- testplans/spid-cie-oidc/implementations/spid-cie-oidc-django/build_and_run.sh
+++ testplans/spid-cie-oidc/implementations/spid-cie-oidc-django/build_and_run.sh
@@ -10,12 +10,12 @@ cp ./edited_files/Dockerfile ./spid-cie-oidc-django/
 cd spid-cie-oidc-django
 bash docker-prepare.sh
 
-# local build i-mig-t --------
-cd ../../../../../tools/i-mig-t
-# rm mig-t-beta-jar-with-dependencies.jar
-sudo docker build -t i-mig-t .
-cd ../../testplans/spid-cie-oidc/implementations/spid-cie-oidc-django/spid-cie-oidc-django/
-# local build i-mig-t --------
+# # local build i-mig-t --------
+# cd ../../../../../tools/i-mig-t
+# # rm mig-t-beta-jar-with-dependencies.jar
+# sudo docker build -t i-mig-t .
+# cd ../../testplans/spid-cie-oidc/implementations/spid-cie-oidc-django/spid-cie-oidc-django/
+# # local build i-mig-t --------
 
 xhost +local:
 sudo docker compose up --remove-orphans
